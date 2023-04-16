dc0626e395f863ff63794ae68c2d1a8eff0ce297
TempoQuill <jicrapo@gmail.com>
2023-04-15 21:22:02 -0400

    Add title theme

---
+++ music/titlescreen.asm
@@ -0,0 +1,586 @@
+Music_TitleScreen:
+	channel_count 4
+	channel 1, Music_TitleScreen_Ch1
+	channel 2, Music_TitleScreen_Ch2
+	channel 3, Music_TitleScreen_Ch3
+	channel 4, Music_TitleScreen_Ch4
+
+Music_TitleScreen_Ch1:
+	tempo 123
+	volume 7, 7
+	duty_cycle 2
+	stereo_panning TRUE, TRUE
+	note_type 4, 10, 1
+	octave 2
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	volume_envelope 9, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	volume_envelope 8, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	volume_envelope 7, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	volume_envelope 6, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	volume_envelope 5, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	volume_envelope 4, 1
+	note A#, 1
+	note B_, 1
+	octave 4
+	note C_, 1
+	volume_envelope 3, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	duty_cycle 1
+	note_type 3, 12, 3
+	octave 3
+	note G_, 5
+	rest 3
+	note G_, 7
+	rest 1
+	note F_, 7
+	rest 1
+	note E_, 6
+	rest 2
+	note C_, 8
+	duty_cycle 2
+	volume_envelope 8, 1
+	octave 5
+	note F_, 2
+	note G_, 2
+	note A_, 2
+	note B_, 2
+	octave 6
+	note C_, 2
+	rest 6
+	stereo_panning FALSE, TRUE
+	volume_envelope 10, 2
+	octave 3
+	note E_, 4
+	rest 4
+	stereo_panning TRUE, TRUE
+	duty_cycle 1
+	volume_envelope 12, 3
+	note D#, 6
+	rest 2
+	note D#, 7
+	rest 1
+	note C_, 7
+	rest 1
+	note C_, 7
+	rest 1
+	octave 2
+	note G#, 10
+	duty_cycle 2
+	volume_envelope 8, 1
+	octave 5
+	note D#, 2
+	note F_, 2
+	note G_, 2
+	note G#, 4
+	rest 12
+	rest 8
+	duty_cycle 1
+	volume_envelope 12, 3
+	octave 3
+	note G#, 6
+	rest 10
+	note G#, 5
+	rest 11
+	note A_, 5
+	rest 3
+	note A_, 10
+	rest 6
+	note D#, 5
+	rest 3
+	note D#, 6
+	rest 2
+	note E_, 7
+	rest 1
+	note F#, 5
+	rest 3
+	note G#, 13
+	rest 3
+	note F#, 7
+	rest 9
+	rest 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 10, 2
+	note C_, 4
+	rest 12
+	note C_, 4
+	rest 12
+	note C_, 4
+	rest 4
+	note C_, 9
+	rest 7
+	rest 8
+	note C_, 4
+	rest 12
+	note C_, 5
+	rest 11
+	note G_, 3
+	rest 5
+	note G_, 10
+	rest 6
+	rest 8
+	note G_, 3
+	rest 13
+	note G_, 5
+	rest 11
+	note G_, 3
+	rest 13
+	note G_, 3
+	rest 5
+	stereo_panning TRUE, TRUE
+	duty_cycle 1
+	volume_envelope 12, 3
+	octave 2
+	note B_, 8
+	duty_cycle 2
+	volume_envelope 8, 1
+	octave 5
+	note G_, 2
+	note B_, 2
+	octave 6
+	note D_, 2
+	note G_, 2
+	duty_cycle 1
+	volume_envelope 12, 3
+	octave 2
+	note C_, 8
+	duty_cycle 2
+	volume_envelope 8, 1
+	note B_, 2
+	octave 6
+	note D_, 2
+	note G_, 2
+	duty_cycle 1
+	volume_envelope 12, 3
+	octave 3
+	note E_, 4
+	note F_, 8
+	note_type 12, 11, 4
+	note E_, 12
+	sound_ret
+
+Music_TitleScreen_Ch2:
+	stereo_panning TRUE, TRUE
+	duty_cycle 2
+	note_type 4, 10, 1
+	octave 2
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	volume_envelope 9, 1
+	note A#, 1
+	note B_, 1
+	octave 3
+	note C_, 1
+	volume_envelope 8, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	volume_envelope 7, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	volume_envelope 6, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	volume_envelope 5, 1
+	note A#, 1
+	note B_, 1
+	octave 4
+	note C_, 1
+	volume_envelope 4, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	volume_envelope 3, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	duty_cycle 1
+	note_type 3, 14, 2
+	note C_, 6
+	rest 2
+	octave 3
+	note B_, 5
+	rest 3
+	note A_, 7
+	rest 1
+	note G_, 6
+	rest 2
+	note_type 6, 13, 3
+	note E_, 11
+	rest 1
+	note_type 3, 14, 2
+	note G_, 4
+	rest 4
+	note G#, 7
+	rest 1
+	note G_, 8
+	note F_, 4
+	rest 4
+	note D#, 8
+	note_type 6, 13, 3
+	note C_, 10
+	rest 2
+	stereo_panning FALSE, TRUE
+	duty_cycle 2
+	note_type 3, 10, 2
+	note C_, 5
+	rest 3
+	rest 8
+	stereo_panning TRUE, TRUE
+	duty_cycle 1
+	volume_envelope 14, 2
+	octave 4
+	note E_, 6
+	rest 10
+	note E_, 5
+	rest 11
+	note D#, 5
+	rest 3
+	note D#, 9
+	rest 7
+	octave 3
+	note B_, 5
+	rest 3
+	note B_, 6
+	rest 2
+	octave 4
+	note C#, 7
+	rest 1
+	note D#, 5
+	rest 3
+	note E_, 13
+	rest 3
+	note D_, 7
+	rest 9
+	duty_cycle 2
+	note_type 6, 11, 3
+	octave 6
+	note C_, 4
+	octave 5
+	note B_, 4
+	note A_, 4
+	note G_, 4
+	note E_, 12
+	note G_, 4
+	octave 6
+	note C_, 4
+	octave 5
+	note B_, 4
+	note A_, 4
+	note A#, 4
+	note B_, 16
+	note B_, 4
+	note G_, 4
+	note A_, 4
+	note G_, 4
+	note B_, 4
+	note G_, 4
+	note A_, 4
+	note G_, 4
+	duty_cycle 1
+	volume_envelope 14, 2
+	octave 3
+	note G_, 4
+	note G_, 3
+	rest 1
+	note A_, 4
+	note B_, 4
+	octave 4
+	note C_, 2
+	note D_, 4
+	note_type 12, 13, 3
+	note E_, 12
+	sound_ret
+
+Music_TitleScreen_Ch3:
+	stereo_panning TRUE, TRUE
+	note_type 4, 1, 4
+	octave 3
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	volume_envelope 2, 4
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	volume_envelope 3, 4
+	octave 4
+	note C_, 1
+	note C#, 1
+	note D_, 1
+	note D#, 1
+	note E_, 1
+	note F_, 1
+	note F#, 1
+	note G_, 1
+	note G#, 1
+	note A_, 1
+	note A#, 1
+	note B_, 1
+	stereo_panning TRUE, TRUE
+	note_type 3, 1, 0
+	octave 3
+	note C_, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 4
+	note C_, 4
+	rest 4
+	sound_call .sub1
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note C_, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 4
+	note C_, 8
+	sound_call .sub1
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note G#, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note G#, 7
+	rest 1
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note D#, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note G#, 8
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note G#, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note G#, 8
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note D#, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note G#, 3
+	rest 5
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note E_, 6
+	rest 2
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	note B_, 5
+	rest 3
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	note C#, 6
+	rest 2
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	note B_, 5
+	rest 3
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note B_, 6
+	rest 2
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note A_, 4
+	rest 4
+	note A_, 15
+	rest 1
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note B_, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note A_, 6
+	rest 2
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	note C#, 5
+	rest 3
+	note D#, 5
+	rest 3
+	note E_, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	note B_, 3
+	rest 5
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	note D_, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	note A_, 4
+	rest 4
+	note C_, 8
+	note B_, 3
+	rest 5
+	octave 2
+	note G_, 7
+	rest 1
+	octave 3
+	note B_, 3
+	rest 5
+	note C_, 7
+	rest 1
+	note B_, 4
+	rest 4
+	note B_, 9
+	rest 7
+	note C_, 8
+	note B_, 4
+	rest 4
+	octave 2
+	note G_, 8
+	octave 3
+	note B_, 4
+	rest 4
+	note G_, 8
+	octave 4
+	note F_, 4
+	rest 4
+	note F_, 10
+	rest 6
+	octave 3
+	note G_, 7
+	rest 1
+	octave 4
+	note F_, 3
+	rest 5
+	octave 3
+	note D_, 8
+	octave 4
+	note F_, 5
+	rest 3
+	octave 3
+	note G_, 8
+	octave 4
+	note F_, 3
+	rest 5
+	octave 3
+	note D_, 8
+	octave 4
+	note F_, 2
+	rest 6
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note G_, 5
+	rest 3
+	note G_, 4
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 3
+	note F_, 4
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note F#, 5
+	rest 3
+	note F_, 6
+	rest 2
+	note E_, 3
+	rest 1
+	note D_, 4
+	rest 4
+	note_type 12, 1, 0
+	note C_, 12
+	sound_ret
+
+.sub1:
+	stereo_panning TRUE, TRUE
+	volume_envelope 1, 4
+	octave 2
+	note G_, 8
+	stereo_panning FALSE, TRUE
+	volume_envelope 1, 4
+	octave 4
+	note C_, 4
+	rest 4
+	sound_ret
+
+Music_TitleScreen_Ch4:
+	toggle_noise 1
+	stereo_panning TRUE, TRUE
+	drum_speed 12
+	rest 8
+.loop1:
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 2
+	drum_note 8, 2
+	sound_loop 3, .loop1
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 4
+	drum_note 8, 4
+.loop2:
+	drum_note 8, 2
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 1
+	drum_note 8, 1
+	drum_note 8, 2
+	drum_note 8, 2
+	sound_loop 3, .loop2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 2
+	drum_note 8, 4
+	rest 7
+	sound_ret
